f26d7c864011a5a056e2180187c959a2f98eb6d7
ADRIENA <muhammadadrmkti@gmail.com>
2024-07-17 23:01:54 +0700

    update commir

--- Tugas-Besar-Aplikasi-Laundry-UploadFile-Java/Database/db_uas_laundry.sql
+++ Tugas-Besar-Aplikasi-Laundry-UploadFile-Java/Database/db_uas_laundry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2024 at 05:40 PM
+-- Generation Time: Jul 17, 2024 at 06:00 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,9 +43,6 @@ CREATE TABLE `customer` (
 
 INSERT INTO `customer` (`number`, `id_user`, `username`, `nama`, `password`, `telpon`, `alamat`) VALUES
 (14, 'CST14', 'mukti1', 'mukti aja', '123', '3344', 'bdg'),
-(15, 'CST15', 'adri', 'adriena', 'adri', '099', 'bjr'),
-(16, 'CST16', 'adriena', 'adriena muk', 'adriena1', '11122', 'bjr'),
-(17, 'CST17', 'riena', 'riena adri', '123', '0099', 'bjrs'),
 (5, 'CST5', 'ka', 'kaka', '234', '0182309812345', 'dyh');
 
 -- --------------------------------------------------------
@@ -140,12 +137,7 @@ CREATE TABLE `tab_pesanan` (
 --
 
 INSERT INTO `tab_pesanan` (`no`, `id_pesanan`, `tanggal`, `jam`, `id_user`, `nama`, `no_hp`, `alamat`, `services`, `berat`, `aktual_berat`, `ongkir`, `harga`, `diskon`, `total_bayar`, `status`) VALUES
-(13, '#ORD-0013', '10/07/2024', '8', 'CST14', 'mukti aja', '3344', 'bdg', 'Cuci Setrika', 1, 1, 15000, 9000, 0, 24000, 'false'),
-(14, '#ORD-0014', '08/07/2024', '8', 'CST15', 'adriena', '099', 'bjr', 'Cuci Kering', 12, 12, 15000, 7000, 0, 99000, 'false'),
-(15, '#ORD-0015', '08/07/2024', '11', 'CST16', 'adriena muk', '11122', 'bjr', 'Cuci Kering', 12, 12, 0, 7000, 0, 84000, 'false'),
-(16, '#ORD-0016', '08/07/2024', '11', 'CST17', 'riena adri', '0099', 'bjrs', 'Cuci Kering', 12, 12, 0, 7000, 0, 84000, 'false'),
-(17, '#ORD-0017', '12/07/2024', '10', 'CST15', 'adriena', '099', 'bjr', 'Cuci Kering', 12, 12, 15000, 7000, 0, 99000, 'false'),
-(18, '#ORD-0018', '13/07/2024', '10', 'CST17', 'riena adri', '0099', 'bjrs', 'Cuci Setrika', 12, 12, 15000, 9000, 0, 123000, 'false');
+(13, '#ORD-0013', '10/07/2024', '8', 'CST14', 'mukti aja', '3344', 'bdg', 'Cuci Setrika', 1, 1, 15000, 9000, 0, 24000, 'false');
 
 --
 -- Indexes for dumped tables
